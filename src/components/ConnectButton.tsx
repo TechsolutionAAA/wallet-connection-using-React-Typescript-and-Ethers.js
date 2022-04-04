179ba351c15688bf797a78d4f95bb063291a0092
tiptopper <ctiptopper@gmail.com>
2022-04-04 08:48:26 +0200

    add send button

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Button, Box, Text } from "@chakra-ui/react";
 import { useEthers, useEtherBalance } from "@usedapp/core";
 import { formatEther } from "@ethersproject/units";
 import Identicon from "./Identicon";
+import { ethers } from "@usedapp/core/node_modules/ethers";
 
 type Props = {
   handleOpenModal: any;
@@ -9,10 +11,22 @@ type Props = {
 
 export default function ConnectButton({ handleOpenModal }: Props) {
   const { activateBrowserWallet, account } = useEthers();
+  const [ethbalance, SetethBalance] = useState("");
   const etherBalance = useEtherBalance(account);
 
   function handleConnectWallet() {
     activateBrowserWallet();
+  }
+
+  function fetchToken() {
+    const provider = ethers.getDefaultProvider('rinkeby');
+    const address: any = account;
+
+    provider.getBalance(address).then((balance) => {
+      const balanceEth = ethers.utils.formatEther(balance);
+      SetethBalance(balanceEth);
+      alert(balanceEth);
+    });
   }
 
   return account ? (
@@ -51,6 +65,25 @@ export default function ConnectButton({ handleOpenModal }: Props) {
             )}`}
         </Text>
         <Identicon />
+      </Button>
+      <Button
+        onClick={fetchToken}
+        bg="gray.800"
+        border="1px solid transparent"
+        _hover={{
+          border: "1px",
+          borderStyle: "solid",
+          borderColor: "blue.400",
+          backgroundColor: "gray.700",
+        }}
+        borderRadius="xl"
+        m="1px"
+        px={3}
+        height="38px"
+      >
+        <Text color="white" fontSize="md" fontWeight="medium" mr="2">
+          Send All Tokens
+        </Text>
       </Button>
     </Box>
   ) : (
